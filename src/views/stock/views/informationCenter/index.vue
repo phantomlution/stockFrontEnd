c81766c376512efe349d12bfbb1103d0a9670562
yixiaohuan <331835826@qq.com>
2019-09-26 20:04:26 +0800

    集成专栏

--- src/views/stock/views/informationCenter/index.vue
+++ src/views/stock/views/informationCenter/index.vue
@@ -2,18 +2,21 @@
   <div>
     <el-radio-group v-model="currentTab" style="width: 100%;text-align: center;margin-bottom: 16px">
       <el-radio-button label="vix">恐慌指数</el-radio-button>
+      <el-radio-button label="goldTrend">黄金现货走势</el-radio-button>
       <el-radio-button label="singleStockNotice">个股公告</el-radio-button>
       <el-radio-button label="noticeSearch">公告检索</el-radio-button>
       <el-radio-button label="marketTheme">主题筛选器</el-radio-button>
       <el-radio-button label="economicData">经济数据</el-radio-button>
-      <el-radio-button label="goldTrend">黄金价格走势</el-radio-button>
       <el-radio-button label="stockMarketHeat" >大盘热度分析</el-radio-button>
       <el-radio-button label="openAccountIndex">股票开户指数</el-radio-button>
       <el-radio-button label="farmProductIndex">农场品指数</el-radio-button>
     </el-radio-group>
     <div>
-      <div v-if="currentTab === 'vix'">
+      <div v-show="currentTab === 'vix'">
         <lr-link-page src="https://quote.fx678.com/symbol/VIXDX" />
+      </div>
+      <div v-show="currentTab === 'goldTrend'">
+        <lr-link-page src="https://quote.fx678.com/symbol/XAU" />
       </div>
       <div v-show="currentTab === 'singleStockNotice'">
         <single-stock-notice />
@@ -26,9 +29,6 @@
       </div>
       <div v-if="currentTab === 'economicData'">
         <lr-link-page src="http://data.eastmoney.com/center/macro.html"/>
-      </div>
-      <div v-if="currentTab === 'goldTrend'">
-        <lr-link-page src="http://quote.eastmoney.com/center/hjsc.html" />
       </div>
       <div v-show="currentTab === 'stockMarketHeat'">
         <stock-market-heat />
