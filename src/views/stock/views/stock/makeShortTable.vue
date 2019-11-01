512da3156b4dac1df959ab2065b94876916b61a9
yixiaohuan <331835826@qq.com>
2019-11-01 10:52:53 +0800

    分析组件解耦

--- src/views/stock/views/stock/makeShortTable.vue
+++ src/views/stock/views/stock/makeShortTable.vue
@@ -1,47 +1,7 @@
 <template>
   <lr-box style="position: relative">
     <!-- 工具条 -->
-    <lr-stick-bar title="选股助手" width="600" top="128px">
-      <div>
-        <div style="display: flex">
-          <template v-if="currentRowModel">
-            <div style="flex: 1">
-              <div style="margin-bottom: 8px">
-                <span style="font-weight: bold;font-size: 16px">
-                  <lr-stock-detail-link :add="true" :code="currentRowModel.code" :name="currentRowModel.name" />
-                  &nbsp;
-                  <template v-if="currentRowIndex !== -1">
-                    {{ currentRowIndex + 1 }}/{{ dataList.length }}
-                  </template>
-                </span>
-              </div>
-              <div v-if="currentRowModel.themeList.length > 0">
-                <el-tag v-for="(theme, themeIndex) of currentRowModel.themeList" :key="themeIndex">
-                  {{ theme }}
-                </el-tag>
-              </div>
-            </div>
-            <div style="margin: 0 16px;">
-              <div>
-                <div class="lr-make-short-table-label">低于最高：{{ currentRowModel.closeMaxIncrement }}%</div>
-              </div>
-              <div>
-                <div class="lr-make-short-table-label">高于最低：{{ currentRowModel.closeMinIncrement }}%</div>
-              </div>
-            </div>
-            <div>
-              <div><el-button type="text" @click.stop="toPrevious">上一条</el-button></div>
-              <div><el-button type="text" @click.stop="toNext">下一条</el-button></div>
-            </div>
-          </template>
-          <template v-else>
-            <div style="display: flex;justify-content: center;align-items: center;width: 100%;">
-              <el-button type="primary" @click.stop="setRowIndex(0)">查看第一条</el-button>
-            </div>
-          </template>
-        </div>
-      </div>
-    </lr-stick-bar>
+    <stock-assistant ref="stockAssistant" />
     <div>
       <el-form :inline="true">
         <el-form-item label="总记录数">
@@ -66,7 +26,7 @@
       <el-table-column label="code">
         <template slot-scope="props">
           <span>(L{{ props.row.rank}},{{ props.row.diffIncrement }}%){{ props.row.code }}({{ props.row.name}}),{{ props.row.targetDate }},{{ props.row.profit }}%</span>
-          <el-button type="text" @click.stop="setRowIndex(props.$index)">查看</el-button>
+          <el-button type="text" @click.stop="showDetail(props.row.code)">查看</el-button>
           <span>{{ props.row.bounceRate }}</span>
         </template>
       </el-table-column>
@@ -92,8 +52,12 @@
 <script>
 import lodash from 'lodash'
 import { STOCK_PRICE_MIN, STOCK_PRICE_MAX } from '@/utils/stock'
+import stockAssistant from './stockAssistant.vue'
 
 export default {
+  components: {
+    stockAssistant
+  },
   data() {
     return {
       flowReturn: false, // 是否考虑流量回归
@@ -101,8 +65,6 @@ export default {
       hasEverLimitUp: true,
       overMinPercent: 5, // 高于最低值百分比
       dataList: [],
-      currentRowModel: null,
-      currentRowIndex: -1,
       defaultSort: {
         prop: 'closeMaxIncrement',
         order: 'ascending'
@@ -121,22 +83,10 @@ export default {
     },
     overMinPercent() {
       this.refresh()
-    },
-    currentRowIndex(rowIndex) {
-      if (rowIndex === -1) {
-        this.currentRowModel = null
-      } else {
-        // 获取表格最终渲染的顺序
-        const currentRow = this.$refs.table.tableData[rowIndex]
-        this.$refs.table.setCurrentRow(currentRow)
-        this.showDetail(currentRow.code)
-        this.currentRowModel = currentRow
-      }
     }
   },
   mounted() {
     this.$bus.$on('analyzeMakeShort', data => {
-      this.currentRowIndex = -1
       /**
        * 分级
        */
@@ -211,29 +161,18 @@ export default {
       this.dataList.forEach(item => {
         secondPhaseMap.set(item.code, item)
       })
+
+      this.$nextTick(_ => {
+        // 获取表格最终渲染的顺序
+        const finalTableData = this.$refs.table.tableData
+        this.$refs.stockAssistant.init(finalTableData)
+      })
     })
   },
   beforeDestroy() {
     this.$bus.$off('analyzeMakeShort')
   },
   methods: {
-    setRowIndex(rowIndex) { // 设置当前行数
-      this.currentRowIndex = rowIndex
-    },
-    toPrevious() {
-      if (this.currentRowIndex === 0) {
-        return this.$message.warning('已经是第一条了')
-      } else if (this.currentRowIndex <= this.dataList.length - 2) {
-        this.currentRowIndex -= 1
-      }
-    },
-    toNext() {
-      if (this.currentRowIndex === this.dataList.length - 1) {
-        return this.$message.warning('已经是最后一条了')
-      } else {
-        this.currentRowIndex += 1
-      }
-    },
     refresh() {
       this.$bus.$emit('restartAnalyzeProbability')
     },
---
+++ src/views/stock/views/stock/stockAssistant.vue
@@ -0,0 +1,88 @@
+<template>
+  <lr-stick-bar title="选股助手" width="600" top="128px">
+    <div>
+      <div style="display: flex">
+        <template v-if="currentRowModel">
+          <div style="flex: 1">
+            <div style="margin-bottom: 8px">
+                <span style="font-weight: bold;font-size: 16px">
+                  <lr-stock-detail-link :add="true" :code="currentRowModel.code" :name="currentRowModel.name" />
+                  &nbsp;
+                  <template v-if="currentRowIndex !== -1">
+                    {{ currentRowIndex + 1 }}/{{ dataList.length }}
+                  </template>
+                </span>
+            </div>
+            <div v-if="currentRowModel.themeList.length > 0">
+              <el-tag v-for="(theme, themeIndex) of currentRowModel.themeList" :key="themeIndex">
+                {{ theme }}
+              </el-tag>
+            </div>
+          </div>
+          <div style="margin: 0 16px;">
+            <div>
+              <div class="lr-make-short-table-label">低于最高：{{ currentRowModel.closeMaxIncrement }}%</div>
+            </div>
+            <div>
+              <div class="lr-make-short-table-label">高于最低：{{ currentRowModel.closeMinIncrement }}%</div>
+            </div>
+          </div>
+          <div>
+            <div><el-button type="text" @click.stop="toPrevious">上一条</el-button></div>
+            <div><el-button type="text" @click.stop="toNext">下一条</el-button></div>
+          </div>
+        </template>
+        <template v-else>
+          <div style="display: flex;justify-content: center;align-items: center;width: 100%;">
+            <el-button type="primary" @click.stop="setRowIndex(0)">查看第一条</el-button>
+          </div>
+        </template>
+      </div>
+    </div>
+  </lr-stick-bar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentRowModel: null,
+      currentRowIndex: -1,
+      dataList: []
+    }
+  },
+  watch: {
+    currentRowIndex(val) {
+      if (val >= 0 && val < this.dataList.length) {
+        this.currentRowModel = this.dataList[val]
+      } else {
+        this.currentRowModel = null
+      }
+
+    }
+  },
+  methods: {
+    init(dataList) {
+      this.dataList = dataList
+      this.currentRowIndex = -1
+    },
+    setRowIndex(index) {
+      this.currentRowIndex = index
+    },
+    toPrevious() {
+      if (this.currentRowIndex === 0) {
+        return this.$message.warning('已经是第一条了')
+      } else if (this.currentRowIndex < this.dataList.length) {
+        this.currentRowIndex -= 1
+      }
+    },
+    toNext() {
+      if (this.currentRowIndex === this.dataList.length - 1) {
+        return this.$message.warning('已经是最后一条了')
+      } else {
+        this.currentRowIndex += 1
+      }
+    },
+  }
+}
+</script>
